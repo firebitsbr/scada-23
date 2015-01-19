0b8799e9331d2b977471d91e85daf3a9e5857fb5
Thomas Rooney <trrooney@frontier.com>
2015-01-18 19:33:26 -0500

    Initial commit of tstatreader_pin_definitions.h File defines the pins for sensor interface and serial communication using the sensor interface board thermostat_reader_001

---
+++ thermostat_reader/tstatreader_pin_definitions.h
@@ -0,0 +1,20 @@
+#ifndef MH_SAC_TSTATREADER_PIN_DEFINITIONS_H_INCLUDED
+#define MH_SAC_TSTATREADER_PIN_DEFINITIONS_H_INCLUDED
+
+// this file defines the pins and devices required for sensor interface and serial communication
+// using the sensor interface board thermostat_reader_001
+// sensor interface can be any two-wire protocol.
+// serial communication is via RS-485.
+
+const int numDevices = 8;
+
+// input, output and control pins for each device
+const int rxPin = 0;
+const int txPin = 1;
+const int dePin = 2;
+const int notRePin = 3;
+
+const int dataPins[numDevices] =  {4, 6, 8, 10, 12, 14, 16, 18};
+const int clockPins[numDevices] = {5, 7, 9, 11, 13, 15, 17, 19};
+
+#endif // MH_SAC_TSTATREADER_PIN_DEFINITIONS_H_INCLUDED
